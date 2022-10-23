21929a6cea9988131e9b553969397d4e250c0aee
Soham Raut <sohamr@cs.washington.edu>
2022-10-23 09:24:02 -0700

    coupon search react

--- dubhacks-2022/src/Components/Home.js
+++ dubhacks-2022/src/Components/Home.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router';
 import {Redirect} from 'react-router-dom';
+import {searchCoupon} from '../backend/recipes.js';
 
 
 
 // displays coupons
 function Cards(props) {
-  
+
     if(props.data.length < 1) {
       return <h1 className="no-favs">No coupons for this item available. Please try again at a later date!</h1>
     }
     else {
       // the props for this should be the state values, specifically the card object array, and the gridView
       // based on the listView/GridView thing, rerender the cards accordingly
-    
+
         return props.data.map((currentCard) => {
-          
+
           return(
           <div className="cardpoolGrid" key={currentCard.name_product}>
             <CardGridView key={currentCard.name_product} card={currentCard} />
           </div>);
         });
-      
+
     }
   }
 
@@ -29,8 +30,8 @@ function Cards(props) {
 
 function CardGridView(props) {
     let navigate = useNavigate();
-    let url = "/recipes/" + props.card.name_product.split(" ").join("").toString();  
-   
+    let url = "/recipes/" + props.card.name_product.split(" ").join("").toString();
+
     return(
       <div  className="card" >
         <img className="card-img-top" src={props.card.image} alt={props.card.name_product} onClick= {() => {navigate(url)}}/>
